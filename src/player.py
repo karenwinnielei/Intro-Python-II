a63250ed897e1445ab8a47f9fe8a98b9e4f9c067
Karen Lei <karenwinnielei@gmail.com>
2020-08-06 22:58:37 -0700

    complete MVP

--- src/player.py
+++ src/player.py
@@ -5,6 +5,20 @@ class Player:
   def __init__(self, name, current_room):
     self.name = name
     self.current_room = current_room
+    self.items = []
   
   def __str__(self):
-    return f"{self.name} {self.current_room}"
+    return f"Your items: {self.items}"
+
+  def move(self, direction):
+    directions = ['n', 's', 'w', 'e']
+    if direction not in directions:
+      next_direction = input('Please enter a valid direction [`n`, `s`, `w`, `e`]')
+      self.move(next_direction)
+
+  def retrieve(self, item):
+    self.items.append(item)
+    self.print_items()
+  
+  def print_items(self):
+    print("Your items: ", [f"{item.name}" for item in self.items], '\n')
--- src/room.py
+++ src/room.py
@@ -2,8 +2,22 @@
 # description attributes.
 
 class Room:
-  def __init__(self, name, description):
+  def __init__(self, name, description, items=[]):
     self.name = name
     self.description = description
+    self.items = items
+
+  def get_item(self, item):
+    del self.items[item]
+  
+  def add_item(self, item):
+    if self.items:
+      self.items.append(item)
+    return self.items
+  
   def __str__(self):
-    return f"{self.name} {self.description}"
+    return f'{self.name}: {self.description}'
+  
+  def print_item(self):
+    for name, description in enumerate(self.items):
+      print(f"{name}: {description}")
